0e115e86483ba646983f11c37432d1a39bfbb3de
Kash <kashm768@gmail.com>
2022-02-05 23:59:14 -0600

    Initial Commit

---
+++ dataypes.py
@@ -0,0 +1,47 @@
+#Clear the terminal screen
+import os
+os.system('cls')
+
+#################################
+# DATA TYPES
+# Strings
+# Numbers
+# Lists
+# Tuples
+# Dictionaries
+# Boolean
+################################
+
+# Strings
+first_name = "Prakash"
+last_name = 'Reddy'
+print (first_name)
+print (last_name)
+
+# Numbers
+age = 39
+print (age)
+
+# Lists
+names = ["Prakash", "Veni", "Rikin", "Rithya"]
+
+print(names)
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+
+# Dictionaries
+fav_pizza = {
+"Prakash":"Pepperoni",
+"Veni":"Mushroom", 
+"Rikin":"Chicken",
+"Rithya":"Veggie" 
+}
+
+print (fav_pizza["Veni"])
+
+# Boolean
+name = True
+print(name)
+
---
+++ hello.py
@@ -0,0 +1,30 @@
+import os
+os.system('cls')
+
+#Hello World text comment
+print("Hello World!")  #Its another Comment
+
+'''
+Multi Line Comment
+Line 1
+Line 2
+Line 3
+'''
+# Variables
+full_name = "Prakash Reddy"
+print(full_name)
+
+first_name = "Prakash"
+print(first_name)
+
+first_name = "Reddy"
+print(first_name)
+
+last_name = "Reddy"
+first_name = last_name
+print(first_name)
+
+a = 5
+b = 6
+c = a + b
+print(c)
